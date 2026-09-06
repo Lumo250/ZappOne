e92124cea25fca4752e89fd1205fb9ca5ddbf217
AG Design <lumo250@libero.it>
2026-09-06 12:31:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // basta cambiare una cifra per forzare il refresh su tutti i client.
 // ============================================================
 
-const BUILD_TIME  = '20250327-0019'; // <-- aggiorna ad ogni deploy
+const BUILD_TIME  = '20260906-2420'; // <-- aggiorna ad ogni deploy
 const CACHE_NAME  = 'ZappOne-cache-' + BUILD_TIME;
 
 

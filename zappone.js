3404069f47c0676f9d96728b984e8c56c04d4962
AG Design <lumo250@libero.it>
2026-09-22 16:41:01 +0200

    Update zappone.js

--- zappone.js
+++ zappone.js
@@ -4894,7 +4894,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 // 5. Feedback all'utente (usiamo appConfig per sicurezza)
     if (appConfig.isPremium) {
-        showNotification("ZappOne Premium Attivo! V26.0X");
+        showNotification("ZappOne Premium Attivo! V26.0Y");
     } else {
         showNotification("Modalità Demo attiva", false);
     }

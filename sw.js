313ca45123488fffbe7ff565bd90bba47bcb6362
AG Design <lumo250@libero.it>
2026-09-26 16:29:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // basta cambiare una cifra per forzare il refresh su tutti i client.
 // ============================================================
 
-const BUILD_TIME  = '20260926-1007'; // <-- aggiorna ad ogni deploy (bump per: estrazione modulo epg.js)
+const BUILD_TIME  = '20260927-1007'; // <-- aggiorna ad ogni deploy (bump per: estrazione modulo epg.js)
 const CACHE_NAME  = 'ZappOne-cache-' + BUILD_TIME;
 
 
